c0219f54818f2fad48d12e9ad38004200725d634
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-13 17:56:04 -0700

    Changing base price and value error

--- melons.py
+++ melons.py
@@ -1,4 +1,6 @@
 """Classes for melon orders."""
+import random
+import datetime
 
 class AbstractMelonOrder(object):
     tax = 0
@@ -10,27 +12,48 @@ class AbstractMelonOrder(object):
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.base_price = self.set_base_price()
+
+        if self.qty >= 100:
+            raise TooManyMelonsError()
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
 
+    def set_base_price(self):
+        """ determines base price """
+        base_price = random.randint(5,9)
+        now = datetime.datetime.now()
+
+        if 8 <= now.hour < 11 and 0 <= now.weekday() <= 4:
+            # adds $4 during rush hour (8-11) on weekdays
+            base_price += 4
+
+        return base_price
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.base_price()
 
         if self.species == "christmas":
             base_price *= 1.5
 
+        total = (1 + self.tax) * self.qty * base_price
+
         if self.order_type == "international" and self.qty < 10:
-            total = (1 + self.tax) * self.qty * base_price + 3
-        else:
-            total = (1 + self.tax) * self.qty * base_price
+            total += 3
 
         return total
 
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order from the US Govt"""
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
@@ -54,3 +77,11 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class TooManyMelonsError(ValueError):
+    # def __init__(self):
+        # self.value = value
+        # self.msg = msg
+    pass
+
+
